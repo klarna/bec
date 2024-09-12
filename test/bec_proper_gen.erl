d88432b34b4ef9803c9b376961af9a484f2dad65
Jesper Eskilson <jesper.eskilson@klarna.com>
2024-09-12 13:27:21 +0200

    Allow skipping API calls for setting repos public

--- test/bec_proper_gen.erl
+++ test/bec_proper_gen.erl
@@ -439,7 +439,15 @@ config_keys() ->
 config_value('project')             -> bec_test_utils:bitbucket_project_key();
 config_value('repo')                -> bec_test_utils:bitbucket_repo_slug();
 config_value('default-branch')      -> branch_id();
-config_value('public')              -> bool();
+config_value('public')              ->
+  case os:getenv("BEC_SKIP_PUBLIC") of
+    Value when is_list(Value) ->
+      io:format(standard_error, "Skipping PUBLIC", []),
+      [];
+    false ->
+      io:format(standard_error, "Not skipping PUBLIC", []),
+      bool()
+  end;
 config_value('users')               -> ss_permission_users();
 config_value('groups')              -> ss_permission_groups();
 config_value('branch-restrictions') -> ss_branch_restrictions();
